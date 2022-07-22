a6f11f7c1a5c33dad686491894457ce47c9b89cf
lore <lore>
2022-07-22 21:38:35 +0800

    Update bash

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-kubectl apply -f local-storage/local-storage.yaml
-kubectl apply -f local-storage/katib-mysql.yaml
-kubectl apply -f local-storage/authservice-pvc.yaml
-kubectl apply -f local-storage/minio-pvc.yaml
-kubectl apply -f local-storage/mysql-pv-claim.yaml
-while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+sudo kubectl apply -f local-storage/local-storage.yaml
+sudo kubectl apply -f local-storage/katib-mysql.yaml
+sudo kubectl apply -f local-storage/authservice-pvc.yaml
+sudo kubectl apply -f local-storage/minio-pvc.yaml
+sudo kubectl apply -f local-storage/mysql-pv-claim.yaml
+while ! kustomize build example | sudo kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
